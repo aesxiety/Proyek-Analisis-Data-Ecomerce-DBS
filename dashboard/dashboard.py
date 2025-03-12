b51bdc9b9d52088d347d482ed79c66b63189771b
aesxiety <rezaariyadi8@gmail.com>
2025-03-12 09:37:58 +0800

    update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -42,9 +42,7 @@ def created_orders_canceled(df):
     return cancellation_by_hour
 
 
-all_df = pd.read_csv("../all_data.csv")
-# url = "https://github.com/aesxiety/Proyek-Analisis-Data-Ecomerce-DBS/blob/main/dashboard/all_data.csv"
-# all_df = pd.read_csv(url)
+all_df = pd.read_csv("ecomerce.csv")
 
 datetime_columns = ["order_purchase_timestamp", "order_delivered_customer_date"]
 all_df.sort_values(by="order_purchase_timestamp", inplace=True)
